dfaa4cccb3c8a39f6179303bc49f8cc155c9109f
Phil541 <philfilippou4@gmail.com>
2021-12-07 20:05:25 +1100

    Day 3 p2 solution + Day 4 input, Python

--- Python-lang/Day 3/day3_v2.py
+++ Python-lang/Day 3/day3_v2.py
@@ -12,24 +12,55 @@ gamma = []
 epsilon =[] 
 sumtracker = [0] * len(input[0]) #frequency tracker for each position
 
-
-
-for c in range(0,len(input[0])): #iterate through each column
+input02 = input.copy()
+inputc02 = input.copy()
+for c in range(0,len(input02[0])): #iterate through each column
+    i=0
     sumtracker = 0 #reset sumtracker for each column
-    for r in range(0,len(input)): #Iterate through each row count number of occurances of '1'
-        sumtracker += int(input[r][c])
+    for r in range(0,len(input02)): #Iterate through each row count number of occurances of '1'
+        sumtracker += int(input02[r][c])
 
-    if sumtracker >= len(input)/2: #check if '1' is most common value at specific column
-        print(c)
-        for r in input: #iterate through rows and delete rows without '1' at specific column
-            if r[c] != '1':
-                input.remove(r)
-    else:
-        print(c)
-        for r in input: #iterate through rows and delte rows without '0' at specific column
-            if r[c] != '0':
-                input.remove(r)
+    if sumtracker >= len(input02)/2: #check if '1' is most common value at specific column
+        for r in range(0,len(input02)): #iterate through rows and delete rows without '1' at specific column
+            if r-i > len(input02)-1 or len(input02)==1:
+                break
+            if input02[r-i][c] != '1':
+                del input02[r-i]
+                i += 1
+    else:     
+        for r in range(0,len(input02)): #iterate through rows and delte rows without '0' at specific column
+            if r-i > len(input02)-1 or len(input02)==1:
+                break
+            if input02[r-i][c] != '0':
+                del input02[r-i]
+                i += 1
 
-print(input)
 
 
+for c in range(0,len(inputc02[0])): #iterate through each column
+    i=0
+    sumtracker = 0 #reset sumtracker for each column
+    for r in range(0,len(inputc02)): #Iterate through each row count number of occurances of '1'
+        sumtracker += int(inputc02[r][c])
+
+    if sumtracker >= len(inputc02)/2: #check if '1' is most common value at specific column
+        for r in range(0,len(inputc02)): #iterate through rows and delete rows without '1' at specific column
+            if r-i > len(inputc02)-1 or len(inputc02)==1:
+                break
+            if inputc02[r-i][c] != '0':
+                del inputc02[r-i]
+                i += 1
+    else:     
+        for r in range(0,len(inputc02)): #iterate through rows and delte rows without '0' at specific column
+            if r-i > len(inputc02)-1 or len(inputc02)==1:
+                break
+            if inputc02[r-i][c] != '1':
+                del inputc02[r-i]
+                i += 1
+
+print(input02)
+print(inputc02)
+
+
+lifesupport = int(input02[0],2) * int(inputc02[0],2)
+print(lifesupport)
